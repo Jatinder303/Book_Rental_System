0f6bbf9376d67733258b807fe6bd027931616519
Jatinder Singh <Jatinder303@gmail.com>
2021-01-18 15:14:11 +1300

    Add project files.

---
+++ Dashboard.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Book_Rental_System
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public string UserName;
+        public Dashboard(string userName)
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            UserName = userName;
+            lblName.Content = "Hi " + UserName;
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Book_Rental_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+        public string UserName;
+        public string Password;
+        SqlConnection SqlConn = new SqlConnection("Data Source=DPKASTG-05\\SQLEXPRESS;Initial Catalog=LibrarySystem;Integrated Security=True");
+
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            UserName = txtUserName.Text;
+            Password = password.Text;
+            LoginCheck();
+
+        }
+
+        private void LoginCheck()
+        {
+            SqlCommand SqlStr = new SqlCommand();
+            SqlDataReader SqlReader;
+            string SqlStmt;
+
+            try
+            {
+                SqlStr.Connection = SqlConn;
+                SqlStmt = "Select * from Login where username = '" + UserName + "' and password = '" + Password + "'";
+                SqlStr.CommandText = SqlStmt;
+                SqlConn.Open();
+                SqlReader = SqlStr.ExecuteReader();
+
+                if (SqlReader.HasRows)
+                {
+                    MessageBox.Show("Login Successful");
+
+                    Dashboard dash = new Dashboard(UserName);
+                    dash.UserName = UserName;
+                    dash.Show();
+                    SqlConn.Close();
+                    Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Check Username/Password");
+                    txtUserName.Clear();
+                    password.Clear();
+                    SqlConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database Exception" + ex.Message);
+                SqlConn.Close();
+            }
+        }
+    }
+}
+
